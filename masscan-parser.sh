0ac61d6cecabec69c00ee38bc888cf3ff5dd01ea
Sam <52508285+sammooredev@users.noreply.github.com>
2021-02-08 21:12:06 -0800

    Delete masscan-parser.sh

--- masscan-parser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ports=$(cat $1 | awk '{print $4}' | cut -d / -f 1)
-
-ips=$(cat $1 | awk '{print $6}' | sed 's/$/:/')
-
-combined=$(paste <(echo "$ips") <(echo "$ports") | sed -r 's/\s+//g')
-
-printf "$combined\n"
